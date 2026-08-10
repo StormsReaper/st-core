95422d2afc281fb70c346a26ffbd7d016fc29568
Chris Storms <95435318+StormsReaper@users.noreply.github.com>
2026-08-10 13:12:03 -0700

    Initialize st-core resource

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'st-core'
+author 'StormsReaper'
+description 'Storms Technologies core framework for FiveM.'
+version '0.1.0'
+
+lua54 'yes'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
