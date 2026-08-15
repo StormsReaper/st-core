e2b3818d6e3c8e6278d47ee2a0e55332ad61eebd
Chris Storms <95435318+StormsReaper@users.noreply.github.com>
2026-08-15 15:54:02 -0700

    Load unified MDT API

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,7 +9,7 @@ ui_page 'html/index.html'
 shared_scripts {'config.lua','shared/validation.lua'}
 client_scripts {'client.lua','client/dmv.lua','client/sales.lua','integrations/jg-dealerships/client.lua'}
 server_scripts {
- '@oxmysql/lib/MySQL.lua','server.lua','server/payments.lua','server/documents.lua','server/purchases.lua','server/vehicles.lua','server/vehicle_history.lua','server/titles.lua','server/licenses.lua','server/insurance.lua','server/insurance_claims.lua','server/insurance_cron.lua','server/dmv_services.lua','server/enforcement.lua','server/dmv.lua','server/sales.lua','integrations/jg-dealerships/server.lua'
+ '@oxmysql/lib/MySQL.lua','server.lua','server/payments.lua','server/documents.lua','server/purchases.lua','server/vehicles.lua','server/vehicle_history.lua','server/titles.lua','server/licenses.lua','server/insurance.lua','server/insurance_claims.lua','server/insurance_cron.lua','server/dmv_services.lua','server/enforcement.lua','server/mdt.lua','server/dmv.lua','server/sales.lua','integrations/jg-dealerships/server.lua'
 }
 files {'html/index.html','html/style.css','html/app.js','html/contract-hotkeys.js'}
 dependencies {'oxmysql','ox_inventory'}
